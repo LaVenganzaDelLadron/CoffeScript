b7f30bd795c69f8239244b1ecd54861397774d8d
argus <eltonofficial403@gmail.com>
2025-09-24 00:56:18 +0800

    Initial Commit Again

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+import uuid
+
+from fastapi import FastAPI, HTTPException, Depends, status, UploadFile, File
 from pydantic import BaseModel
 from typing import Annotated
+from sqlalchemy import text, func
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy.orm import Session
 import models
 from database import engine, SessionLocal
+from models import CoffeeStatus
 
 # Create tables
 models.Base.metadata.create_all(bind=engine)
@@ -51,6 +56,37 @@ async def login(user: UserLogin, db: db_dependency):
     if not db_user or db_user.password != user.password:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     return {"message": "Login successfully", "user_id": db_user.AID}
+
+
+@app.post("/addcoffee/")
+async def add_coffee(coffee: CoffeeStatus, db: db_dependency, name, description, category, price, aid, file: UploadFile = File(...)):
+    db_coffee = db.query(models.AddCoffee).filter(models.AddCoffee.name == coffee.name).first()
+    if db_coffee:
+        raise HTTPException(status_code=400, detail="Coffee is already exists")
+
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+
+
+    result = db.execute(text("SELECT GenerateCoffeeID()")).scalar()
+    new_id = result
+
+    new_coffee = models.AddCoffee(
+        id=new_id,
+        name=name,
+        description=description,
+        category=category,
+        price=price,
+        aid=aid,
+        image=contents
+    )
+
+    db.add(new_coffee)
+    db.commit()
+    db.refresh(new_coffee)
+
+    return {"message": "Coffee Added Successfully", "coffee_id":new_coffee.id}
+
 
 
 
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, LargeBinary, DECIMAL, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+
 from database import Base
+import enum
 
 class Admin(Base):
     __tablename__ = "admin"
@@ -7,7 +10,24 @@ class Admin(Base):
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
+    coffees = relationship("AddCoffee", back_populates="admin")
 
-class Coffees(Base):
+
+class CoffeeStatus(enum.Enum):
+    active = "active"
+    inactive = "inactive"
+
+class AddCoffee(Base):
     __tablename__ = "coffees"
+    id = Column(String(50), primary_key=True, index=True)
+    name = Column(String(50))
+    description = Column(String(255))
+    category = Column(String(255))
+    price = Column(DECIMAL(10, 2), nullable=False)
+    status = Column(Enum(CoffeeStatus), default=CoffeeStatus.active)
+    image = Column(LargeBinary)
+    aid = Column(Integer, ForeignKey("admin.AID", ondelete="CASCADE"))
+
+    admin = relationship("Admin", back_populates="coffees")
+
 
